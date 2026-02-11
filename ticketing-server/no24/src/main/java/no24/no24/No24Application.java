0725fd6b1cfef9cb55c3b9f3da0526f9fdce92b2
jinkyeongkimm <jinkyeong.kim@boldplaygames.com>
2026-02-12 00:18:52 +0900

    ticketing-server 추가

---
+++ ticketing-server/no24/src/main/java/no24/no24/No24Application.java
@@ -0,0 +1,13 @@
+package no24.no24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class No24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(No24Application.class, args);
+	}
+
+}
---
+++ ticketing-server/no24/src/test/java/no24/no24/No24ApplicationTests.java
@@ -0,0 +1,13 @@
+package no24.no24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class No24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
